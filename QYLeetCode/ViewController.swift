8fcfcc417fb255f06fdd5172df07bd5cbb62aae1
qusy <qushiyu@qutoutiao.net>
2021-12-07 15:59:49 +0800

    init

---
+++ QYLeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QYLeetCode
+//
+//  Created by qusy on 2021/12/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
